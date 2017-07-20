cc5fcb31ae7ab7ef6419bb675974c544b86f2cca
Duc Dao <ducdao11@gmail.com>
2017-07-20 15:37:37 -0700

    Added GET all cases for a specific user and POST a new case for a specific user.

--- src/caseInfo.py
+++ src/caseInfo.py
@@ -3,13 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import reqparse, abort, Api, Resource
 
 class Case(Resource):
+   # Get all cases for some specific user
    def get(self):
-      
+      # Get JSON data from frontend containing userId
+      data = request.get_json()
 
+      case = CaseSummary.query.filter_by(userId=data['userId']).all()
+
+      return { "json_list": [i.serialize for i in case] }
+   
+   # Create a new case
    def post(self):
       # Get JSON data from frontend
       data = request.get_json()
-      
+
       # Create new case object, populated with values from JSON data
       case = CaseSummary(
          caseId = data['id'],
