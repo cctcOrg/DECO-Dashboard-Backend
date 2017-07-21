361e5fa5794cc91c6315670c7e0033074500d0b6
Jackson Kurtz <jkurtz678@gmail.com>
2017-07-21 11:27:00 -0700

    added all of the endpoints to flaskServ (not tested with json yet, so probably has tons of errors).

--- src/flaskServ.py
+++ src/flaskServ.py
@@ -3,14 +3,14 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 from flask import Response, jsonify, request, redirect, render_template, url_for
 from sqlalchemy import create_engine
-from databaseSetup import FileUpload, User, Overview, EvidenceSummary, MediaStatus, DeviceDetails, DigitalMediaDesc, ImagingInformation, NetworkCollectionInfo, CloudCollectionInfo, Notes  
+from databaseSetup import Users, CaseSummary, DeviceDesc, DigitalMediaDesc, ImageInfo, RelevantFiles  
 from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS
 import os
 from werkzeug.utils import secure_filename
 import datetime
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/dbnew'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/dashboarddb'
 app.debug=True
 api = Api(app)
 db = SQLAlchemy(app)
@@ -21,24 +21,138 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class UserInfo(Resource):
   # create a new user 
-  def post(self):
-    data = request.get_json()
-    user = User( 
-        email = data['email'],
-        lastName = data['lastName'],
-        firstName = data['firstName'] )
-    #stage and commit to database
-    db.session.add( user)
-    db.session.commit()
-    return 200
+    def post(self):
+        data = request.get_json()
+        users = Users( 
+            email = data['email'],
+            lastName = data['lastName'],
+            firstName = data['firstName'] )
+        #stage and commit to database
+        db.session.add( users)
+        db.session.commit()
+        return 200
 
-  def get(self):
-    #query User table by email 
-    info = db.session.query(User).filter_by(email = request.args.get('email')).first()
-    #return Response( json.dumps(info.id, info.firstName), mimetype='application/json' )
-    return jsonify( id=info.id, email=info.email, firstName=info.firstName, lastName=info.lastName)
+    def get(self):
+        #query User table by email 
+        info = db.session.query(Users).filter_by(email = request.args.get('email')).first()
+        return jsonify( id=info.id, email=info.email, firstName=info.firstName, lastName=info.lastName)
 
-class SaveFileFS(Resource):
+class Case(Resource):
+   # Return case summary
+    def get(self):
+        # Get JSON data from frontend containing userId
+        data = request.get_json()
+
+        # Get a specific case for logged in user
+        if (caseId == request.args.get('caseId')):
+            case = CaseSummary.query.filter_by(id = caseId, userId=data['userId']).all()
+        # Get all cases for logged in user
+        else:
+            case = CaseSummary.query.filter_by(userId=data['userId']).all()
+
+        return { "json_list": [i.serialize for i in case] }
+   
+    # Create a new case
+    def post(self):
+        # Get JSON data from frontend
+        data = request.get_json()
+
+        # Create new case object, populated with values from JSON data
+        user = db.session.query(Users).filter_by( id = data['userId']).one()
+        case = CaseSummary(
+            id = data['id'],
+            dateReceived = data['dateReceived'],
+            caseNumber = data['caseNumber'],
+            caseDescription = data['caseDescription'],
+            deviceDesc = data['deviceDesc'],
+            suspectName = data['suspectName'],
+            collectionLocation = data['collectionLocation'],
+            examinerNames = data['examinerNames'],
+            users = user )
+
+        # Stage case for commit to database
+        db.session.add( case )
+
+        # Commit case to database
+        db.session.commit()
+
+        return 200
+
+class Device( Resource):
+    def get(self):
+            data = request.get_json() 
+            info = db.session.query(DeviceDescription).filter_by(caseSummaryId = data['caseSummaryId']).all()
+            return { "json_list": [i.serialize for i in info] }
+
+    def post( self ):
+        data = request.get_json()
+        users = db.session.query(Users).filter_by( id = data['userId']).one()
+        case_summary = db.session.query(CaseSummary).filter_by(id = data['caseId'] ).one()
+        deviceDesc=DeviceDesc( 
+                deviceDescription = data['deviceDescription'],
+                make = data['make'],
+                model = data['model'],
+                serialNumber = data['serialNumber'],
+                deviceStatus = data['deviceStatus'],
+                shutDownMethod = data['shutDownMethod'],
+                systemDateTime = data['systemDateTime'],
+                localDateTime = data['localDateTime'],
+                typeOfCollection = data['typeOfCollection'],
+                mediaStatus = data['mediaStatus'],
+                users = users,
+                case_summary = case_summary
+                )
+        db.session.add( deviceDesc )
+        db.session.commit()
+
+class Media(Resource):
+    def post(self):
+      data = request.get_json()
+      users = db.session.query( Users).filter_by( id = data['userId']).one()
+      deviceDesc = db.session.query( deviceDesc ).filter_by( id = data['deviceId']).one()
+      media = DigitalMediaDesc (
+            storageId = data['storageId'],
+            make = data['make'],
+            model = data['model'],
+            serialNumber = data['serialNumber'],
+            capacity = data['capacity'],
+            users = users,
+            device_desc = device_desc )
+      db.session.add( media )
+      db.session.commit()
+ 
+    def get(self):
+      data = request.get_json();
+      media = db.session.query( DigitalMediaDesc ).filter_by(deviceDescId = data['deviceDescId']).all()
+      return { "digital media list": [i.serialize for i in media] }
+
+class Image( Resource):
+    def get(self):
+        data = request.get_json()
+        info = db.session.query(ImageInfo).filter_by(digitalMediaDescId=data['digitalMediaDescId']).one()
+        return { "json_list": [i.serialize for i in info ] }
+
+    def post( self ):
+        data = request.get_json()
+        users = db.session.query(Users).filter_by( id = data['userId']).one()
+        digital_media_desc = db.session.query(DigitalMediaDesc).filter_by( id = data['mediaId']).one()
+        imageInfo = ImageInfo( 
+                writeBlockMethod = data['writeBlockMethod'], 
+                imagingTools = data['imagingTools'],
+                format = data['format'],
+                primaryStorageMediaId = data['primaryStorageMediaId'],
+                primaryStorageMediaName = data['primaryStorageMediaName'],
+                backupStorageMediaId = data['backupStorageMediaId'],
+                backupStorageMediaName = data['backupStorageMediaName'],
+                postCollection = data['postCollection'],
+                size = data['size'],
+                users=users,
+                digital_media_desc = digital_media_desc 
+                )
+        db.session.add( imageInfo )
+        db.session.commit()
+
+class File(Resource):
     def post(self):
         # Get file name
         if 'file' not in request.files:
@@ -66,30 +180,13 @@ class SaveFileFS(Resource):
             db.session.commit()
             return 200
 
-class Media(Resource):
-    def post(self):
-      data = request.get_json()
-      media = DigitalMediaDesc (
-            storageId = data['storageId'],
-            make = data['make'],
-            model = data['model'],
-            serialNumber = data['serialNumber'],
-            capacity = data['capacity'] 
-            users = users
-            deviceDesc = device_description )
-      db.session.add( media )
-      db.session.commit()
- 
-    def get(self):
-      data = request.get_json();
-      media = db.session.query( DigitalMediaDesc ).filter_by(deviceDescId = data['deviceDescId']).all()
-      return { "digital media list": [i.serialize for i in media] }
 
 api.add_resource( UserInfo, '/evd/user')
-api.add_resource( Media, '/evd/media')
-api.add_resource( Form, '/evd/form')
-api.add_resource( First, '/evd/first')
-api.add_resource( SaveFileFS, '/evd/upload')
+api.add_resource( Case, '/evd/case')
+api.add_resource( Device, '/evd/dev')
+api.add_resource( Media, '/evd/dm')
+api.add_resource( Image, '/evd/img')
+api.add_resource( File, '/evd/file')
 
 if __name__ == "__main__":
     app.run( host = app.run( host = '129.65.247.21', port = 5000), debug=True )
